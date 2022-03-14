939622c69f61daf29b465d2d068d2a44596da4f5
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-14 12:47:40 +0530

    Added UC1

---
+++ src/com/bridgelabz/day7/GamblerProgram.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day7;
+    //UC1
+    public class GamblerProgram {
+        public static void main(String[] args) {
+            System.out.println("Welcome to Gambling Program");
+        }
+
+    public void UC1() {
+        int startingTotalAmount=100;
+        int betsPerGame=1;
+        }
+}
